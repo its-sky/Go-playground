fbf91b0b2f1107ac71086270fbf8e154225cd8ba
its-sky <smc9919@naver.com>
2024-09-02 13:56:35 +0900

    feat: type을 통한 delegation 연습

--- src/practice/firstclass/firstclass.go
+++ src/practice/firstclass/firstclass.go
@@ -10,9 +10,19 @@ func main() {
 
 	r2 := calc(func(x int, y int) int { return x - y }, 10, 20)
 	println(r2)
+
+	r3 := calc2(func(x int, y int) int { return x * y }, 100, 200)
+	println(r3)
 }
 
 func calc(f func(int, int) int, a int, b int) int {
+	result := f(a, b)
+	return result
+}
+
+type calculator func(int, int) int
+
+func calc2(f calculator, a int, b int) int {
 	result := f(a, b)
 	return result
 }
